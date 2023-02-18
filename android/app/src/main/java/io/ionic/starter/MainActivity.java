08e2ccd3c2da6759bfa28447524613ba8bb22543
David Martínez <david.martinez@polypoint.ch>
2023-02-18 19:51:14 +0100

    Set up more plugins and add icons.

---
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mimacom.devicecontrol;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'lunchAndLearn',
+  appId: 'com.mimacom.devicecontrol',
+  appName: 'Device Control',
   webDir: 'www',
   bundledWebRuntime: false
 };
