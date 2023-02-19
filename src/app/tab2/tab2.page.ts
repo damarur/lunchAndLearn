54bbab47efc5d5ca0cd3dc3148966417b2b3cb11
David Martínez <david.martinez@polypoint.ch>
2023-02-19 19:35:01 +0100

    Camera and Filesystem integration.

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Platform} from '@ionic/angular';
+import {Camera, CameraResultType, Photo} from '@capacitor/camera';
+import {Directory, Filesystem} from '@capacitor/filesystem';
+import {isMobile} from '../util/platform/platform.util';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +11,69 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  public imageUrl;
 
+  constructor(private readonly platform: Platform) {
+    if (isMobile(this.platform)) {
+      Camera.requestPermissions();
+    }
+  }
+
+  async takePicture() {
+    const capturedPhoto = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri
+    });
+
+    console.log(capturedPhoto);
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    this.imageUrl = capturedPhoto.webPath;
+
+    // Can be set to the src of an image now
+    // imageElement.src = imageUrl;
+
+    // Save the picture
+    this.savePicture(capturedPhoto);
+  }
+
+  private async savePicture(photo: Photo) {
+    // Convert photo to base64 format, required by Filesystem API to save
+    const base64Data = await this.readAsBase64(photo);
+
+    // Write the file to the data directory
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
+    });
+
+    // Use webPath to display the new image instead of base64 since it's
+    // already loaded into memory
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath
+    };
+  }
+
+  private async readAsBase64(photo: Photo) {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Platform} from '@ionic/angular';
+import {Camera, GalleryPhoto, GalleryPhotos} from '@capacitor/camera';
+import {Filesystem, ReadFileResult} from '@capacitor/filesystem';
+import {isMobile} from '../util/platform/platform.util';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +11,33 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  public imagesInBase64 = [];
 
+  constructor(private readonly platform: Platform) {
+    if (isMobile(this.platform)) {
+      Camera.requestPermissions();
+    }
+  }
+
+  async choosePictures() {
+    this.imagesInBase64 = [];
+    await Camera.pickImages({
+      quality: 90,
+      width: 600,
+      height: 600,
+      correctOrientation: true,
+      limit: 3
+    }).then((galleryPhotos: GalleryPhotos) => {
+      galleryPhotos.photos.forEach((photo: GalleryPhoto) => {
+        console.log(photo);
+        this.readFile(photo.path);
+      })
+    });
+  }
+
+  readFile(path: string) {
+    Filesystem.readFile({path}).then((file: ReadFileResult) => {
+      this.imagesInBase64.push('data:image/jpeg;base64,' + file.data);
+    });
+  }
 }
