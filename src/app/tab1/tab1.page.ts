a24c6a602d46f9e80b6a7b1f395872719a7e2914
David Martínez <david.martinez@polypoint.ch>
2023-02-19 13:43:18 +0100

    Changed splash. Added permission for google play. Change tab 1. Delete unused component.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Device, DeviceInfo} from '@capacitor/device';
+import {BatteryInfo} from "@capacitor/device/dist/esm/definitions";
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,26 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public deviceInfo: DeviceInfo | undefined;
+  public batteryInfo: BatteryInfo = {};
+
+  constructor() {
+    this.logDeviceInfo();
+    this.logBatteryInfo();
+  }
+
+  public logDeviceInfo() {
+    Device.getInfo().then(deviceInfo => {
+      console.log(deviceInfo);
+      this.deviceInfo = deviceInfo;
+    });
+  };
+
+  public logBatteryInfo() {
+    Device.getBatteryInfo().then(batteryInfo => {
+      console.log(batteryInfo);
+      this.batteryInfo = batteryInfo
+    });
+  };
 
 }
